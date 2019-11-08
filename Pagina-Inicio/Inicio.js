50c5431e91a54300e5add77db0bc66d01b3e0030
areyes26 <areyesabarca@udesa.edu.ar>
2019-11-08 10:48:56 -0300

    Series Populares
    pop

--- Pagina-Inicio/Inicio.js
+++ Pagina-Inicio/Inicio.js
@@ -1,3 +1,6 @@
+window.onload = function(){
+
+//SERIES POPULARES
 fetch("https://api.themoviedb.org/3/tv/popular?api_key=2e2296c9e03da266b3fa417a70458299&language=en-US&page=1")
   .then(function(response) {
       return response.json();
@@ -12,6 +15,9 @@ fetch("https://api.themoviedb.org/3/tv/popular?api_key=2e2296c9e03da266b3fa417a7
       }
       */
      for(var i = 0; i < resultadoFinal.results.length; i++) {
-         listado.innerHTML += "<li class='unaClase'>" + resultadoFinal.results[i].original_name + "</li>"
+       console.log(resultadoFinal.results[i]);
+         listado.innerHTML += '<li>'+ '<a href="../detalles-serie/detalles-serie.html">' + '<img src="https://image.tmdb.org/t/p/w300/' + resultadoFinal.results[i].poster_path + '">' + '</a>'+ '</li>'
+
      }
   })
+}
