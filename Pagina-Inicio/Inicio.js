a59640150f3f75b636e0551883c6e23987adc2b2
areyes26 <areyesabarca@udesa.edu.ar>
2019-11-08 11:27:07 -0300

    Listados de Series
    Agregue las tres categorias que pide

--- Pagina-Inicio/Inicio.js
+++ Pagina-Inicio/Inicio.js
@@ -20,4 +20,47 @@ fetch("https://api.themoviedb.org/3/tv/popular?api_key=2e2296c9e03da266b3fa417a7
 
      }
   })
+
+  //SERIES AL AIRE
+  fetch("https://api.themoviedb.org/3/tv/on_the_air?api_key=2e2296c9e03da266b3fa417a70458299&language=en-US&page=1")
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(resultadoFinalAire) {
+        var listadoAire = document.querySelector("#listaAire");
+        //console.log(resultadoFinal.results);
+
+        /*
+        for(var i = 0; i < resultadoFinal.results.length; i++) {
+            console.log(resultadoFinal.results[i].original_name);
+        }
+        */
+       for(var i = 0; i < resultadoFinalAire.results.length; i++) {
+         console.log(resultadoFinalAire.results[i]);
+           listadoAire.innerHTML += '<li>'+ '<a href="../detalles-serie/detalles-serie.html">' + '<img src="https://image.tmdb.org/t/p/w300/' + resultadoFinalAire.results[i].poster_path + '">' + '</a>'+ '</li>'
+
+       }
+    })
+
+    //SERIES MEJOR PUNTUADA
+    fetch("https://api.themoviedb.org/3/tv/top_rated?api_key=2e2296c9e03da266b3fa417a70458299&language=en-US&page=1")
+      .then(function(response) {
+          return response.json();
+      })
+      .then(function(resultadoFinalAire) {
+          var listadoAire = document.querySelector("#listaMejor");
+          //console.log(resultadoFinal.results);
+
+          /*
+          for(var i = 0; i < resultadoFinal.results.length; i++) {
+              console.log(resultadoFinal.results[i].original_name);
+          }
+          */
+         for(var i = 0; i < resultadoFinalAire.results.length; i++) {
+           console.log(resultadoFinalAire.results[i]);
+             listadoAire.innerHTML += '<li>'+ '<a href="../detalles-serie/detalles-serie.html">' + '<img src="https://image.tmdb.org/t/p/w300/' + resultadoFinalAire.results[i].poster_path + '">' + '</a>'+ '</li>'
+
+         }
+      })
+
 }
