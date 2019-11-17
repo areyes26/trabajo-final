1058a0df123e524393169f5f13ab2af27c185f92
areyes26 <areyesabarca@udesa.edu.ar>
2019-11-17 05:00:33 -0300

    Serie por genero con css

--- serie-por-genero/seriePorGenero.js
+++ serie-por-genero/seriePorGenero.js
@@ -7,41 +7,33 @@ var id = query2.get("id")
 var page = 1;
 vermas()
 function vermas() {
-// GENEROSSSSSSS
-var url = "https://api.themoviedb.org/3/discover/tv?api_key=c0e01d0df95b98b689dcb3af16007742&language=en-US&sort_by=popularity.desc&page=2" + page + "&timezone=America%2FNew_York&include_null_first_air_dates=false&with_genres=" + id;
-console.log("hola mundo!");
+
+/*Generos de SERIES*/
+var url = "https://api.themoviedb.org/3/discover/tv?api_key=2e2296c9e03da266b3fa417a70458299&language=en-US&sort_by=popularity.desc&page=2" + page + "&timezone=America%2FNew_York&include_null_first_air_dates=false&with_genres=" + id;
+
 fetch(url)
 .then(function(respuesta){
  return respuesta.json();
 })
 .then(function(datos){
  console.log(datos);
-  var div = document.querySelector(".ul_generos");
+  var div = document.querySelector(".listaGeneros");
   var series = datos.results;
-  //titulo
-  var titulo = document.querySelector("#titulos");
+  /*Titulo*/
+  var titulo = document.querySelector("#tituloSerie");
   var name = query2.get("name")
   titulo.innerHTML = name;
 
   for (var i=0; i<series.length; i++){
-    div.innerHTML += '<li><a id="click_pelis" href="../detalles-serie/detalles-serie.html?id='+ datos.results[i].id +'"> ' + '<img src="https://image.tmdb.org/t/p/w500/' + datos.results[i].poster_path + '">' + '</a></li>'
+    div.innerHTML += '<li><a href="../detalles-serie/detalles-serie.html?id='+ datos.results[i].id +'"> ' + '<img src="https://image.tmdb.org/t/p/w500/' + datos.results[i].poster_path + '">' + '</a></li>'
   }
-console.log(series.length);
-  if (datos.results.length == 0 && page == 1) {
-    titulo.innerHTML = "No se encontró el género";
-    titulo.style.textTransform = "none";
-    titulo.style.padding = "20%"
-    titulo.style.textAlign = "center"
-    titulo.style.color = "Red"
-  }
+
   for(var i=0; i<series.length; i++){
     div.innerHTML+= '<li><a href="../detalles-serie/detalles-serie.html?id='+ datos.results[i].id+'"> '+'<img src="https://image.tmdb.org/t/p/w500/' + datos.results[i].poster_path + '">' + '</a></li>'
   }
   if (datos.total_pages == page) {
-    console.log('cortamo');
     window.removeEventListener('scroll', scrolled)
     return
-    // alert("No se encuentran resultados")
   }
   })
 
