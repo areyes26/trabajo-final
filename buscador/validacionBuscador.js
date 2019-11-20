110de95eaaa946601d2af41a8174c8c963b6d029
Manu <S>
2019-11-20 17:26:40 -0300

    alert 2

--- buscador/validacionBuscador.js
+++ buscador/validacionBuscador.js
@@ -1,14 +1,11 @@
 window.addEventListener("load", function() {
 
-  document.querySelector(".barra-buscadora").onsubmit = function(event) {
-    var loQueEscribio = document.querySelector(".buscadorNomal").value
 
-    if (loQueEscribio.length.value > 3) {
-      event.preventDefault()
-      alert("NO funciona esto")
-        // UIkit.notification({message: 'Notification message'});
-    }
-  }
+  document.querySelector(".barra-buscadora").onsubmit = function(event) {
+    var loQueEscribio = document.querySelector(".buscadorNormal").value
+    event.preventDefault()
+
+console.log(loQueEscribio.length);
 
 if (loQueEscribio.length < 3)
 {
