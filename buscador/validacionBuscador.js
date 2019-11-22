d5a6b7d96ce26d4ad61192dfb95676b40cec56bd
areyes26 <areyesabarca@udesa.edu.ar>
2019-11-22 10:08:35 -0300

    Ari Arreglo error de Alertas

--- buscador/validacionBuscador.js
+++ buscador/validacionBuscador.js
@@ -10,14 +10,14 @@ console.log(loQueEscribio.length);
 if (loQueEscribio.length < 3)
 {
 
-UIkit.notification({
-    message: 'Debes introducir al menos 3 caracteres!',
-    status: 'primary',
-    pos: 'top-right',
-    timeout: 3000
-})
+  UIkit.notification ({
+      message : 'Inserte Al Menos 3 Caracteres',
+      status  : 'primary',
+      timeout : 5000,
+      pos     : 'top-center'
+  });
 //
-// document.querySelector(".barra-buscadora").onsubmit = function() {}
+document.querySelector(".barra-buscadora").onsubmit = function() {}
 //  // {
 //  //
 //  //  UIkit.notification({message: 'Notification message'});
